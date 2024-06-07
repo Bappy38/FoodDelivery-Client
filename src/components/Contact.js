fcf088fd586f021ab0b92f7a4c74a0c3ea671b15
Bappy38 <iqbal.bappy.13@gmail.com>
2024-06-07 16:25:01 +0600

    Contact Us Page Implemented

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,28 @@
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact us Page</h1>
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="font-semibold text-3xl p-4 m-4">Contact Us</h1>
+
+            <form className="p-2 m-2 flex flex-col w-6/12">
+                <input 
+                    type="text" 
+                    className="border border-black p-2 my-2 rounded-sm" 
+                    placeholder="Name"
+                />
+
+                <textarea 
+                    type="text" 
+                    className="border border-black p-2 my-2 rounded-sm" 
+                    placeholder="Write your message"
+                    rows="3"
+                />
+
+                <button 
+                    className="border border-black font-semibold p-2 my-2 text-sm bg-gray-400 text-white rounded hover:bg-gray-700 transition duration-500"
+                >
+                    Submit
+                </button>
+            </form>
         </div>
     )
 }
